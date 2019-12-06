678709fab517a83523b2a282ea0f05c16f07a82d
ImaldiEgyS <173040106@mail.unpas.ac.id>
2019-12-05 17:47:17 -0800

    First commit

---
+++ application/controllers/User.php
@@ -0,0 +1,15 @@
+<?php 
+defined('BASEPATH') or exit ('No direct script acces allowed');
+
+class User extends CI_Controller {
+	public function index() {
+		$data['tittle'] = 'My Profile';
+		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+		
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/sidebar', $data);
+		$this->load->view('templates/topbar', $data);
+		$this->load->view('user/index', $data);
+		$this->load->view('templates/footer');
+	}
+}
